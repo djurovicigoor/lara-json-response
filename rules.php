c2c55b55de3b65e0fd647fc3d92b3b5443901837
Igor <djurovic.igoor@gmail.com>
2018-12-14 20:00:03 +0100

    added git ignore file and removed unnecessary files from project

--- rules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: djurovicigoor, lara-json-response, djurovicigoor, larajsonresponse
-return [
-    'src/MyPackage.php' => 'src/lara-json-response.php',
-    'config/mypackage.php' => 'config/lara-json-response.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/lara-json-response.php',
-    'src/MyPackageServiceProvider.php' => 'src/LaraJsonResponseServiceProvider.php',
-];
